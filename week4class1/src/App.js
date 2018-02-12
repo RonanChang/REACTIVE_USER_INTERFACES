486db03c0aabf0d2cd92b1f69ac0388bad753e8a
RonanChang <ronanch0521@gmail.com>
2018-02-12 14:48:49 +0800

    key props

--- week4class1/src/App.js
+++ week4class1/src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 class App extends Component {
   render() {
     const labels = ["paragraph 1", "paragraph 2", "paragraph 3"];
-    const paragraphs = labels.map(label => {
-      return <p>{label}</p>;
+    const paragraphs = labels.map((label, i) => {
+      return <p key={i}>{label}</p>;
     });
     return <div className="App">{paragraphs}</div>;
   }
