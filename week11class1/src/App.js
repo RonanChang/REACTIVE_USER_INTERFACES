e1fc54e20bfb07d4e01559912a94a25575983c9c
RonanChang <ronanch0521@gmail.com>
2018-04-09 14:40:58 +0800

    css Grid techniques

--- week11class1/src/App.js
+++ week11class1/src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
 
-import "./App.css";
+import "./AppFlex.css";
+import "./AppGrid.css";
 
 class App extends Component {
   render() {
+    /*
+    //For the AppFlex css
     return (
       <div className="App">
         <header>Welcome!</header>
         <div className="wrapper">
+          <aside> sidebar </aside>
+          <main>
+            <p> Content </p>
+            <p> Content </p>
+            <p> Content </p>
+            <p> Content </p>
+          </main>
+        </div>
+      </div>
+    );
+    */
+
+    //for the AppGrid css
+    return (
+      <div className="App">
+        <div className="wrapper">
+          <header>Welcome!</header>
           <aside> sidebar </aside>
           <main>
             <p> Content </p>
