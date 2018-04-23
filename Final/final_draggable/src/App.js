d501a6245fd8fd1d0cb54046ec26dd569fd7ba2f
RonanChang <ronanch0521@gmail.com>
2018-04-23 13:09:41 +0800

    trying draggable

---
+++ Final/final_draggable/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import ContentEditable from "react-contenteditable";
+import "./App.css";
+import Draggable from "react-draggable"; // The default
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { html: "<b>Hello <i>World</i></b>" };
+  }
+
+  handleChange = evt => {
+    this.setState({ html: evt.target.value });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Draggable>
+          <div> Drag here!!! </div>
+        </Draggable>
+
+        <div contentEditable="true"> I can be moved around </div>
+
+        <Draggable>
+          <div contentEditable="true"> I can be moved around </div>
+        </Draggable>
+
+        <Draggable>
+          <input type="text" />
+        </Draggable>
+
+        <Draggable>
+          <ContentEditable
+            html={this.state.html} // innerHTML of the editable div
+            disabled={false} // use true to disable edition
+            onChange={this.handleChange} // handle innerHTML change
+          />
+        </Draggable>
+      </div>
+    );
+  }
+}
+
+export default App;
